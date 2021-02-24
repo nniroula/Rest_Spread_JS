bbb792adc3985eae47da7b805c2e95c4502b15ce
Nabin <nkbhaie2017@gmail.com>
2021-02-24 05:24:02 -0700

    add mergeObjects method

--- rest_spread_ex.js
+++ rest_spread_ex.js
@@ -33,3 +33,16 @@ console.log(findMin(1, 3, 5, 7, 9));
 console.log(findMin(1,4,12,-3));
 console.log(findMin(1,-1));
 console.log(findMin(3,1));
+
+/*
+mergeObjects
+Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys 
+and values of the first object and second object.
+
+mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+*/
+function mergeObjects(obj1, obj2){
+    return {...obj1, ...obj2};
+}
+console.log(mergeObjects({a:1, b:2}, {c:3, d:4}));
